d6506c14314e8b57bcc40d1c53fcccf0863d4af2
Pablo Camacho <mjmbpablo.chilango@gmail.com>
2017-03-17 12:54:23 -0600

    FormatData.py Ver0.02

--- FormatData.py
+++ FormatData.py
@@ -1,6 +1,9 @@
 import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta
 from sqlCont import SqlCont
 from oztools import ContIOTools
+
 
 startDate='2009/02/20';
 endDate='2009/02/23';
@@ -10,83 +13,100 @@ tables_contaminants = oztool.getTables();
 tables_contaminants.extend(oztool.getMeteoTables());
 estations=['MER','TAX','TLA'];
 
-#Auxiliary function to fill the ndarray
-#Receives 3 parameters
-#allData is the ndarray where the information will be store
-#table is the ndarray where the information will be extracted
-#column is the column in the allData
-def completeTable(allData,table,column):
-    cont = 0;
-    for list in table:
-        for x in list:
-            allData[cont,column]= x
-            cont= cont +1;
-    return allData;
 
-
-#function to extract information from the database
-#startDate and endDate range of dates with which the values of the query are extracted
-#estations set the stations to get the values
-def readData(startDate, endDate, estations):
-    sizeData = len(tables_contaminants)*len(estations)+1;
-    conexion= SqlCont();
+def readData(startDate,endDate,estations):
+    """
+    Function to extract information from the database.
+    :param startDate: range of data wit wich the vaues of tue query are extracted.
+    :type startDate: timedata
+    :param endDate : range of data wit wich the vaues of tue query are extracted.
+    :type endDate: timedata
+    :param estations: set the stations to get the values.
+    :type estations : list with the stations
+    :return : allData  all data that was taken from the database.
+    :rtype: DataFrame
+    """
+    conexion = SqlCont();
     conn = conexion.getPostgresConn();
     cur=conn.cursor();
-    cons="""SELECT fecha FROM {0} WHERE id_est ='{1}' AND fecha >= '{2}' AND fecha <= '{3}';""".format(tables_contaminants[1],estations[0],startDate, endDate);
-    cur.execute(cons);
-    date= cur.fetchall();
-    limit = len(date);
-    allData = np.ones((limit,sizeData))*-1;
-    tempData = np.array(date);
-    cont = 1;
-    for x in tables_contaminants:
-        for y in estations:
-            sql ="""SELECT val FROM {0} WHERE id_est = '{1}' AND fecha >= '{2}' AND fecha <= '{3}';""".format(x,y, startDate, endDate);
-            cur.execute(sql);
-            value = cur.fetchall();
-            temValue =  np.array(value);
-            tempAllData = completeTable(allData, temValue,cont)
-            cont = cont +1;
-            allData = tempAllData;
+    #conexion for the database
+    allData= pd.read_sql_query("""SELECT fecha FROM {0} WHERE id_est ='{1}' AND fecha >= '{2}' AND fecha <= '{3}';""".format(tables_contaminants[1],estations[0],startDate, endDate), conn);
+    #query the dates in the gives range
+    numberows = len(allData.index)#Numbers the data given by the previous query
+    for x in estations:
+        for y in tables_contaminants:
+            name = y+"_"+x; #name the column in the DataFrame
+            tempDataValues = pd.read_sql_query("""SELECT val  as {0} FROM {1} WHERE id_est = '{2}' AND fecha >= '{3}' AND fecha <= '{4}';""".format(name,y,x, startDate, endDate),conn);
+            #query the values in the gives range
+            if tempDataValues.empty:
+                #if the query is empty fill it will -1
+                tempData = pd.DataFrame(np.ones((numberows,1))*-1,columns= [name]);
+                allData[name]= tempData;
+            else:
+                allData[name]=tempDataValues;
     conn.commit();
     cur.close();
-    #print allData;
+    #The connection to the database is closed
     return allData;
 
-#readData(startDate,endDate,estations);
 
 
-#function to extract information from the database
-#estations set the stations to get the values
-#contaminant
-#delta number of hour that will add to the time in the consultation
-def builClass(estation,contaminant,delta):
-    contaminantV= oztool.findTable(contaminant);
+
+
+
+def buildClass(allData,estation,contaminant,delta):
+    """
+    Function so that every date in the allData table is added n hours and get the value that belongs to it.
+    :param allData: is data extracted  the function readData
+    :type allData: DataFrame.
+    :param estation: station from wich the data will be taken.
+    :type estation: string.
+    :param contaminant: contaminant from wich the data will be taken.
+    :type contaminant: string.
+    :param delta : number of hours added to the original.
+    :type delta: int, float
+    :return: build
+    :rtype: DataFrame
+    """
     conexion = SqlCont();
     conn = conexion.getPostgresConn();
     cur= conn.cursor();
-    sql="""SELECT fecha + interval '{0} hour' FROM {1} WHERE id_est ='{2}' AND fecha >= '{3}' AND fecha <= '{4}';""".format(delta,contaminantV,estation,startDate, endDate);
-    cur.execute(sql);
-    tempDates = cur.fetchall();
-    size = len(tempDates);
-    build = np.ones((size,2))*-1;
-    dates = np.array(tempDates);
-    cont = 0;
-    for list in dates:
-        for x in list:
-            sql2 = """SELECT val FROM {0} WHERE id_est ='{1}' AND fecha ='{2}';""".format(contaminantV,estation,x);
-            cur.execute(sql2);
-            tempValues = cur.fetchall();
-            values = np.array(tempValues);
-            if len(values)!=0:
-                build[cont,1] = values[0,0];
-                cont=  cont +1;
-            else:
-                build[cont,1]= -1;
-                cont = cont +1;
+    #conexion database
+    tableContaminant = oztool.findTable(contaminant);# name the contaminant in the database
+    fechas = allData['fecha']
+    build= pd.DataFrame(fechas,columns=['fecha']);
+    cont = len(build.index)-1;
+    values = np.ones((cont+1,1))*-1;
+    #building the DataFrame, the first column is the date
+    name = estation + '_'+tableContaminant + '_delta';#name the column in the DataFrame
+    x =0;
+    while x <= cont :
+        #Construction of the second column having the pollutant value
+        time=build.ix[x,'fecha'];
+        time = time + timedelta(hours=delta);#You add the delta to the date
+        sql = """SELECT val FROM {0} WHERE id_est ='{1}' AND fecha ='{2}';""".format(tableContaminant,estation,time);
+        cur.execute(sql);
+        temp=cur.fetchall();
+        tempValue = np.array(temp);
+        if len(tempValue)!=0:
+            #If the query is empty it puts -1 in the value of the column
+            values[x,0]=tempValue[0,0];
+            x=x+1;
+        else:
+            values[x,0]=-1;
+            x=x+1;
+    temBuild= pd.DataFrame(values,columns=[name]);
+    build[name]= temBuild;
     conn.commit();
     cur.close();
-    #print build;
+    #The connection to the database is closed
     return build;
 
-#builClass('TAX','NOX',36);
+def main():
+    dta=readData(startDate,endDate,estations);
+    print(dta);
+    build =buildClass(dta,'TAX','NOX',6);
+    print(build);
+
+if __name__ == "__main__":
+    main()
