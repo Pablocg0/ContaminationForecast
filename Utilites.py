b5586c6c810643bbd4b43ce7a4c9b9347087b9e0
Pablo Camacho <mjmbpablo.chilango@gmail.com>
2017-03-26 19:01:11 -0600

    Utilites.py Ver0.01

---
+++ Utilites.py
@@ -0,0 +1,44 @@
+import FormatData
+import re
+import numpy as np
+from oztools import ContIOTools
+
+def converToArray(alldata,contaminant):
+    """
+    Function that returns in a single column the columns have a specific name
+    :param alldata: matrix with all the data of the database
+    :type alldata: DataFrame
+    :param contaminant: Name of pollutant to look for
+    :type contaminant: string
+    :return: column with all data of the pollutant
+    :rtype; float32
+    """
+    oztool = ContIOTools()
+    name = oztool.findTable(contaminant)
+    c = np.ones((len(alldata.index)*3,1));
+    columns= alldata.columns;
+    patron = re.compile(name+'_.*');
+    index =0;
+    for x in columns:
+        if patron.match(x) != None:
+            temp = alldata[x];
+            for i in temp:
+                if np.isnan(i):
+                    c[index,0]= -1;
+                    index = index +1;
+                else:
+                    c[index,0]= i;
+                    index = index +1;
+    return c;
+
+def normalize_cols(m):
+    """
+    Function for the normalize column
+    :param m: column to Normalize
+    :type m : float32
+    :return : standard column
+    :rtype : float32
+    """
+    col_max = m.max(axis=0)
+    col_min = m.min(axis=0)
+    return (m-col_min) / (col_max - col_min)
