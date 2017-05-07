54cd38b1bee035d294ccb81b620cb06e112ec7b4
Pablo Camacho <mjmbpablo.chilango@gmail.com>
2017-05-07 18:26:15 -0500

    neuralNetworkGpu.py Ver alpha 0.01

--- neuralNetworkGpu.py
+++ neuralNetworkGpu.py
@@ -35,25 +35,25 @@ sess= tf.Session();
 x_vals_train,x_vals_test,y_vals_train,y_vals_test = train_test_split(x_vals, y_vals, test_size=0.2);
 size = len(x_vals_train);# The number of neurons in the first layer is equal to the number of columns of data
 
-with tf.device("/gpu:0"):
-    def init_weight(shape):
-        """
-        Function for the define Variable function weight
-        :param shape: Matrix containing weight
-        :type shape : matrix float32
-        :return: matrix weight
-        """
+def init_weight(shape):
+    """
+    Function for the define Variable function weight
+    :param shape: Matrix containing weight
+    :type shape : matrix float32
+    :return: matrix weight
+    """
+    with tf.device("/gpu:0"):
         weight = tf.Variable(tf.random_normal(shape));
         return weight;
 
-with tf.device("/gpu:1"):
-    def init_bias(shape):
-        """
-        Function for the define Variable function weight
-        :param shape: Matrix containing bias
-        :type shape : matrix float32
-        :return: matrix bias
-        """
+def init_bias(shape):
+    """
+    Function for the define Variable function weight
+    :param shape: Matrix containing bias
+    :type shape : matrix float32
+    :return: matrix bias
+    """
+    with tf.device("/gpu:1"):
         bias=  tf.Variable(tf.random_normal(shape));
         return bias;
 
@@ -62,34 +62,39 @@ with tf.device("/gpu:2"):
     x_data = tf.placeholder(shape=[None,columns-1],dtype=tf.float32);
     y_target= tf.placeholder(shape=[None,1],dtype =tf.float32);
 
-with tf.device("/gpu:4"):
-    def fully_connected(input_layer,weight,biases):
+def fully_connected(input_layer,weight,biases):
+    with tf.device("/gpu:3"):
         layer = tf.add(tf.matmul(input_layer,weight), biases);
         return tf.nn.sigmoid(layer);
 
 #--------Create the first layer (size hidden nodes)--------
 # TODO ya recibe todas las columnas en la primera capa
-weight_1 = init_weight(shape=[columns-1,columns-1]);
-bias_1 = init_bias(shape=[columns-1]);
-layer_1 = fully_connected(x_data,weight_1,bias_1);
+with tf.device("/gpu:0"):
+    weight_1 = init_weight(shape=[columns-1,columns-1]);
+    bias_1 = init_bias(shape=[columns-1]);
+    layer_1 = fully_connected(x_data,weight_1,bias_1);
 
 #--------Create the second layeprint(size);--------
-weight_2 = init_weight(shape=[columns-1,(columns-1)*2]);
-bias_2= init_bias(shape=[(columns-1)*2]);
-layer_2 = fully_connected(layer_1,weight_2, bias_2);
+with tf.device("/gpu:1"):
+    weight_2 = init_weight(shape=[columns-1,(columns-1)*2]);
+    bias_2= init_bias(shape=[(columns-1)*2]);
+    layer_2 = fully_connected(layer_1,weight_2, bias_2);
 
 
 #--------Create output layer (1 output value)--------
-weight_3= init_weight(shape=[(columns-1)*2,1]);
-bias_3 = init_bias(shape=[1]);
-final_output = fully_connected(layer_2,weight_3, bias_3);
+with tf.device("/gpu:2"):
+    weight_3= init_weight(shape=[(columns-1)*2,1]);
+    bias_3 = init_bias(shape=[1]);
+    final_output = fully_connected(layer_2,weight_3, bias_3);
 
 # Declare loss function (L1)
-loss= tf.reduce_mean(tf.abs(y_target - final_output));
+with tf.device("/gpu:0"):
+    loss= tf.reduce_mean(tf.abs(y_target - final_output));
 
 # Declare optimizer gradientDescent
-my_opt = tf.train.GradientDescentOptimizer(0.1);
-train_step = my_opt.minimize(loss);
+with tf.device("/gpu:1"):
+    my_opt = tf.train.GradientDescentOptimizer(0.1);
+    train_step = my_opt.minimize(loss);
 
 # Initialize Variables
 init = tf.global_variables_initializer();
