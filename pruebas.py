b6dc9705b9d7c811ddb96573ac4325bc673c34c2
Pablo Camacho <mjmbpablo.chilango@gmail.com>
2017-05-12 23:39:44 -0500

    pruebas.py ver 0.01

--- pruebas.py
+++ pruebas.py
@@ -21,7 +21,7 @@ def estations():
     print(data);
     build = FormatData.buildClass(data,[estation],contaminant,24);
     total_build = build;
-    xy_values = an.prepro(data,build);
+    xy_values = an.prepro(data,build, contaminant);
     temp_loss = nng.train(xy_values[0],xy_values[1],xy_values[2]);
     loss_vec.append(temp_loss);
     i = 1;
