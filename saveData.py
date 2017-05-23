98fa350d459723b6938daf2ea2abc5b06aa3ffb7
Pablo Camacho <mjmbpablo.chilango@gmail.com>
2017-05-23 10:51:05 -0500

    saveData.py ver 0.03

--- saveData.py
+++ saveData.py
@@ -1,10 +1,20 @@
 from Utilites.FormatData import FormatData as fd
 from Utilites.Utilites import converToArray as ut
+import os
 
 est =['AJM','ATI','BJU','CAM','CCA','CHO','CUA','FAC','IZT','LPR','MER','MGH','NEZ','PED','SAG','SFE','SJA','TAH','TLA','UAX','UIZ','XAL'];
 startDate =['2015/01/01','2013/01/26','1992/11/09','2011/07/01','2014/08/01','2007/07/20','1994/01/02','1990/08/07','2007/07/20','2011/07/05','1986/11/01','2015/01/01','2011/07/27','1986/01/17','1986/02/20','2012/02/20','2011/07/01','1994/01/02','1986/11/01','2012/02/20','1990/05/16','1986/11/22'];
 contaminant = 'O3';
 endDate = '2017/02/01';
+
+if not os.path.exists('data'): os.makedirs('data');
+if not os.path.exists('trainData'): os.makedirs('trainData');
+i=0;
+while i<=21:
+    r = 'trainData/'+est[i];
+    if not os.path.exists(r): os.makedirs(r);
+    i+=1;
+
 
 i = 0;
 while i <= 21:
