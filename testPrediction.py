15bb3bca6fb8edf4e6c964cc60929ddb87b3b28b
Pablo Camacho <mjmbpablo.chilango@gmail.com>
2017-06-12 19:48:19 -0500

    testPrediction.py ver 0.02

--- testPrediction.py
+++ testPrediction.py
@@ -4,26 +4,37 @@ import prediction as pre
 import pandas as df
 import numpy as np
 import matplotlib
-#matplotlib.use('agg')
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 from time import time
 
 
 contaminant = 'O3';
 loss_vec= [];
-est =['AJM','ATI','BJU','CAM','CCA','CHO','CUA','FAC','IZT','LPR','MER','MGH','NEZ','PED','SAG','SFE','SJA','TAH','TLA','UAX','UIZ','XAL'];
-startDate =['2015/01/01','2013/01/26','1992/11/09','2011/07/01','2014/08/01','2007/07/20','1994/01/02','1990/08/07','2007/07/20','2011/07/05','1986/11/01','2015/01/01','2011/07/27','1986/01/17','1986/02/20','2012/02/20','2011/07/01','1994/01/02','1986/11/01','2012/02/20','1990/05/16','1986/11/22'];
+est =['AJM','MGH','CCA','SFE','UAX','CUA','NEZ','CAM','LPR','SJA','CHO','IZT','SAG','TAH','ATI','FAC','UIZ','MER','PED','TLA','BJU','XAL'];
+startDate =['2015/01/01','2015/01/01','2014/08/01','2012/02/20','2012/02/20','2011/10/01','2011/07/27','2011/07/01','2011/07/01','2011/07/01','2007/07/20','2007/07/20','1995/01/01','1995/01/01','1994/01/02','1993/01/01','1987/05/31','1986/01/16','1986/01/16','1986/01/15','1986/01/12','1986/01/10'];
 endDate = '2017/02/01 00:00:00';
 
 
-def trial():
-    sta = est[0]
+def totalPredection():
+    for x in est:
+        print(x);
+        trial(x);
+
+
+def trial(station):
+    sta = station
     d = infor('2016/01/01','2016/12/31',sta,contaminant);
-    data = separateDate(d[0]);
+    dat = d[0]
+    data = separateDate(dat);
+    l = xlabel(dat)
+    labels=l[0];
+    location =l[1];
     build=d[1];
     arrayPred = []
-    nameColumn ='cont_otres_' + sta;
-    inf= data[nameColumn].values
+    nameColumn ='cont_otres_' + sta+'_delta';
+    inf= build[nameColumn].values
     index = data.index.values
     for x in index:
         pred = data.ix[x].values
@@ -32,19 +43,40 @@ def trial():
         arrayPred.append(convert(valNorm));
     result = pre.prediction(sta,contaminant,arrayPred);
     real = desNorm(result,sta,contaminant);
+    plt.figure(figsize=(12.2,6.4))
     plt.plot(inf,'g-', label='Real value');
     plt.plot(real, 'r-',label='Prediction');
-    plt.title('Prediction value');
+    plt.title('NN Predection'+' '+ station +' '+ contaminant);
     plt.xlabel('Days');
     plt.ylabel('PPM');
     plt.legend(loc ='best');
-    plt.savefig('Graficas/Prediction.png',dpi=600);
+    plt.xticks(location,labels,fontsize=6,rotation='vertical');
+    #plt.xlim(0,600)
+    plt.savefig('Graficas/Predicciones/Prediction'+station+ '.png');
     plt.show();
+    plt.clf();
+
 
 def infor(start,end,station,contaminant):
     data = fd.readData(start,end,[station],contaminant);
     build = fd.buildClass2(data,[station],contaminant,24,start,end);
     return [data,build]
+
+def xlabel(data):
+    fechas = [];
+    location=[];
+    dates = data['fecha'];
+    i =0;
+    for x in dates:
+        d =x
+        if d.hour == 0:
+            f = str(d.year) +'/'+ str(d.month)+'/'+str(d.day)
+            fechas.append(f)
+            location.append(i);
+        i+=1;
+    return [fechas,location];
+
+
 
 def convert(data):
     vl = np.ones([1,17]);
@@ -56,11 +88,17 @@ def convert(data):
 
 def desNorm(data,station,contaminant):
     real=[];
+    mini = min(data);
+    maxi = max(data);
+    print(mini)
+    print(maxi)
     name = station+'_'+contaminant;
     values = df.read_csv('data/'+name+'_MaxMin.csv');
     val = values.xs(7).values;
-    maxx = val[2]
-    minn = val[1];
+    maxx = val[2]; #s600
+    minn = val[1]; #-110
+    print(maxx)
+    print(minn)
     for x in data:
         realVal = (x*(maxx-minn))+minn
         real.append(realVal);
@@ -85,7 +123,6 @@ def separateDate(data):
     i  =0 ;
     for x in dates:
         d =x
-        #d = datetime.strptime(x,"%Y-%m-%d %H:%M:%S")
         wD= weekday(d.year,d.month,d.day);
         wDay[i]=wD[0];
         sinWday[i]=wD[1];
@@ -138,4 +175,5 @@ def weekday(year,month,day):
 
 
 #desNorm(est[1],contaminant);
-trial();
+#trial();
+totalPredection();
