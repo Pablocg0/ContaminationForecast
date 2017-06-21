a75dd98bafd84ed36e3ed2229822d4ada519d0fd
Pablo Camacho <mjmbpablo.chilango@gmail.com>
2017-06-21 15:12:23 -0500

    Dominio.py ver0.2

--- NetCDF/Dominio.py
+++ NetCDF/Dominio.py
@@ -11,8 +11,7 @@ def conver1D(array):
         for x in tempData:
             for s in x:
                 array1D.append(s);
-        total.append(array1D);
-        print(len(total));
+        print(array1D);
 
 #net = Dataset('NewFile.nc');
 net = Dataset('/home/pablo/DATA/Dom1_2017-06-13.nc')
@@ -41,7 +40,6 @@ for i in variables:
     var_cut.append(result[0]);
 
 for x in var_cut:
-    print(len(x));
     conver1D(x);
 
 
