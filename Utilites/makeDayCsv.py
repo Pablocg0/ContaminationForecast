8cb9a5df43f9aa9ae5b5aa54de152fc0cd6e5393
Pablo Camacho <pablocg064@gmail.com>
2017-08-01 18:45:46 -0500

    makeDayCsv.py Ver0.1

--- Utilites/makeDayCsv.py
+++ Utilites/makeDayCsv.py
@@ -4,23 +4,31 @@ import pandas as df
 import numpy as np
 
 """
+Function to create csv file with the holidays from 1996 to 2016 from another cvs file
 0 = laboral
-1= oficial
+1 = oficial
 2 = sep
 """
 
 def readCsv():
-    #listDates = makeDates();
-    #print(listDates);
+    """
+    Function to create csv file with the holidays from 1996 to 2016 from another cvs file
+    """
     dataTemp =df.read_csv('data.csv');
     dataTemp = dataTemp.drop(labels=['Unnamed: 0','Unnamed: 1','Unnamed: 23', 'Unnamed: 24'],axis=1);
     data = fillTable(dataTemp);
-    data.to_csv('Festivos.csv', encoding='utf-8');
-    #print(dataTemp);
+    data.to_csv('../data/Festivos.csv', encoding='utf-8');
 
 
 
 def fillTable(dataTemp):
+    """
+    Function to create the csv file with the holidays in the necessary format
+    to add them to the other data of the neural network.
+    :param dataTemp: Information from the original cvs
+    :type dataTemp: DataFrame
+    :return: DataFrame
+    """
     column = dataTemp.columns;
     #print(anio.values);
     value = column[0];
@@ -47,6 +55,12 @@ def fillTable(dataTemp):
 
 
 def hours24(anio):
+    """
+    Function to put the value of a given date in each hour of the day
+    :param anio: Value to repeat for 24 hours
+    :type anio: int
+    :return : int array
+    """
     anioComplete = [];
     for v in anio:
         for i in range(24):
@@ -56,6 +70,12 @@ def hours24(anio):
 
 
 def makeDates(anio):
+    """
+    Function to create the dates
+    :param anio: start year
+    :type anio: int
+    :return datetime array
+    """
     listDates = [];
     date = anio+'-01-01 00:00:00';
     dateInit = datetime.strptime(date,'%Y-%m-%d %H:%M:%S');
@@ -68,6 +88,4 @@ def makeDates(anio):
     listDates.pop();
     return listDates;
 
-
-#makeDates();
 readCsv();
