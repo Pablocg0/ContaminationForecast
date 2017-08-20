0c576feef23cb1096048dca7a49a7ca1a2acee5a
Pablocg0 <pablocg064@gmail.com>
2017-08-20 17:15:16 -0500

    auto.py ver 1.0

--- auto.py
+++ auto.py
@@ -23,6 +23,11 @@ def trainNeuralNetworks(est):
         name = station +'_'+contaminant; #name the file with the data
         data = df.read_csv('data/'+name+'.csv'); #we load the data in the Variable data
         build = df.read_csv('data/'+name+'_pred.csv'); #we load the data in the Variable build
+        data = data[data['fecha']<'2015/12/31'];
+        build = build[build['fecha']<'2015/12/31'];
+        #print(data)
+        #print(build)
+        #break
         xy_values = an(data,build, contaminant); # preprocessing
         nng(xy_values[0],xy_values[1],xy_values[2],1000,station,contaminant); #The neural network is trained
         i+=1;
