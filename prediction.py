d39c7d684a788585942e052793f790283da482b2
Pablo Camacho <mjmbpablo.chilango@gmail.com>
2017-12-11 23:27:14 -0600

    update prediction.py

--- prediction.py
+++ prediction.py
@@ -75,64 +75,54 @@ def fully_connected(input_layer, weight, biases):
 
 
 def prediction(station, contaminant, arrayPred, dirTrain, dirData):
-    name = 'train_' + station + '_' + contaminant + ''
+    """
+    Function to obtain a prediction of a neural network that has
+    already been trained previously
+    :param station: station name for the prediction
+    :type station: string
+    :param contaminant: contaminant for the predictiondate
+    :type contaminant: string
+    :return : value for the prediction
+    """
     result = []
+    name = 'train_' + station + '_' + contaminant + ''
+    data = df.read_csv(dirData + station + '_' + contaminant + '.csv')
+    x_vals = data.values
+    x = x_vals.shape
+    columns = x[1]
+    '#x_vals= x_vals[:,1:columns];'
+
+    x_data = tf.placeholder(shape=[None, columns - 1], dtype=tf.float32)
+    y_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
+    '--------Create the first layer (size hidden nodes)--------'
+    # TODO ya recibe todas las columnas en la primera capa
+    weight_1 = init_weight(shape=[columns - 1, columns - 1])
+    bias_1 = init_bias(shape=[columns - 1])
+    layer_1 = fully_connected(x_data, weight_1, bias_1)
+
+    '--------Create the second layeprint(size);--------'
+    weight_2 = init_weight(shape=[columns - 1, (columns - 1) * 2])
+    bias_2 = init_bias(shape=[(columns - 1) * 2])
+    layer_2 = fully_connected(layer_1, weight_2, bias_2)
+
+    '--------Create output layer (1 output value)--------'
+    weight_3 = init_weight(shape=[(columns - 1) * 2, 1])
+    bias_3 = init_bias(shape=[1])
+    final_output = fully_connected(layer_2, weight_3, bias_3)
+
+    # Declare loss function (L1)
+    loss = tf.reduce_mean(tf.abs(y_target - final_output))
+
+    # Declare optimizer gradientDescent
+    'my_opt = tf.train.GradientDescentOptimizer(0.1)'
+    my_opt = tf.train.AdamOptimizer(0.001)
+    train_step = my_opt.minimize(loss)
     with tf.Session() as sess:
         saver = tf.train.import_meta_graph(dirTrain + station + '/' + name + '.meta')
         saver.restore(sess, tf.train.latest_checkpoint(dirTrain + station + '/'))
+        'print(sess.run(final_output, feed_dict={x_data:values}))'
         for x in arrayPred:
             r = sess.run(final_output, feed_dict={x_data: x})
             result.append(r[0, 0])
         sess.close()
         return result
-    # """
-    # Function to obtain a prediction of a neural network that has
-    # already been trained previously
-    # :param station: station name for the prediction
-    # :type station: string
-    # :param contaminant: contaminant for the predictiondate
-    # :type contaminant: string
-    # :return : value for the prediction
-    # """
-    # result = []
-    # name = 'train_' + station + '_' + contaminant + ''
-    # data = df.read_csv(dirData + station + '_' + contaminant + '.csv')
-    # x_vals = data.values
-    # x = x_vals.shape
-    # columns = x[1]
-    # '#x_vals= x_vals[:,1:columns];'
-    #
-    # x_data = tf.placeholder(shape=[None, columns - 1], dtype=tf.float32)
-    # y_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
-    # '--------Create the first layer (size hidden nodes)--------'
-    # # TODO ya recibe todas las columnas en la primera capa
-    # weight_1 = init_weight(shape=[columns - 1, columns - 1])
-    # bias_1 = init_bias(shape=[columns - 1])
-    # layer_1 = fully_connected(x_data, weight_1, bias_1)
-    #
-    # '--------Create the second layeprint(size);--------'
-    # weight_2 = init_weight(shape=[columns - 1, (columns - 1) * 2])
-    # bias_2 = init_bias(shape=[(columns - 1) * 2])
-    # layer_2 = fully_connected(layer_1, weight_2, bias_2)
-    #
-    # '--------Create output layer (1 output value)--------'
-    # weight_3 = init_weight(shape=[(columns - 1) * 2, 1])
-    # bias_3 = init_bias(shape=[1])
-    # final_output = fully_connected(layer_2, weight_3, bias_3)
-    #
-    # # Declare loss function (L1)
-    # loss = tf.reduce_mean(tf.abs(y_target - final_output))
-    #
-    # # Declare optimizer gradientDescent
-    # 'my_opt = tf.train.GradientDescentOptimizer(0.1)'
-    # my_opt = tf.train.AdamOptimizer(0.001)
-    # train_step = my_opt.minimize(loss)
-    # saver = tf.train.Saver()
-    # with tf.Session() as sess:
-    #     saver.restore(sess, dirTrain + station + '/' + name) '#we load the training'
-    #     'print(sess.run(final_output, feed_dict={x_data:values}))'
-    #     for x in arrayPred:
-    #         r = sess.run(final_output, feed_dict={x_data: x})
-    #         result.append(r[0, 0])
-    #     sess.close()
-    #     return result
