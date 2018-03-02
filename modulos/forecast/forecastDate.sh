07b53939eded91459b421855482903ff991fe089
Pablocg0 <pablocg064@gmail.com>
2018-03-02 10:25:18 -0600

    minor bugs fixes

--- modulos/forecast/forecastDate.sh
+++ modulos/forecast/forecastDate.sh
@@ -3,11 +3,11 @@
 source ~/anaconda3/envs/tensorflow/bin/activate tensorflow
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64/
 python /ServerScript/AirQualityModel/ContaminationForecast/lib/forecastDate.py O3 cont_otres
-#python /ServerScript/AirQualityModel/ContaminationForecast/lib/forecastDate.py PMCO cont_pmco
-#python /ServerScript/AirQualityModel/ContaminationForecast/lib/forecastDate.py PM2.5 cont_pmdoscinco
-#python /ServerScript/AirQualityModel/ContaminationForecast/lib/forecastDate.py NOX cont_nox
-#python /ServerScript/AirQualityModel/ContaminationForecast/lib/forecastDate.py CO cont_co
-#python /ServerScript/AirQualityModel/ContaminationForecast/lib/forecastDate.py NO2 cont_nodos
-#python /ServerScript/AirQualityModel/ContaminationForecast/lib/forecastDate.py NO cont_no
-#python /ServerScript/AirQualityModel/ContaminationForecast/lib/forecastDate.py SO2 cont_sodos
-#python /ServerScript/AirQualityModel/ContaminationForecast/lib/forecastDate.py PM10 cont_pmdiez
+python /ServerScript/AirQualityModel/ContaminationForecast/lib/forecastDate.py PMCO cont_pmco
+python /ServerScript/AirQualityModel/ContaminationForecast/lib/forecastDate.py PM2.5 cont_pmdoscinco
+python /ServerScript/AirQualityModel/ContaminationForecast/lib/forecastDate.py NOX cont_nox
+python /ServerScript/AirQualityModel/ContaminationForecast/lib/forecastDate.py CO cont_co
+python /ServerScript/AirQualityModel/ContaminationForecast/lib/forecastDate.py NO2 cont_nodos
+python /ServerScript/AirQualityModel/ContaminationForecast/lib/forecastDate.py NO cont_no
+python /ServerScript/AirQualityModel/ContaminationForecast/lib/forecastDate.py SO2 cont_sodos
+python /ServerScript/AirQualityModel/ContaminationForecast/lib/forecastDate.py PM10 cont_pmdiez
