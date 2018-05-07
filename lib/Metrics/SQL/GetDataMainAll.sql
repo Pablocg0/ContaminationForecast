f39ad192c644f302d5c341b2a966a1a63983f73d
Olmo Zavala <olmozavala@gmail.com>
2018-05-07 11:01:33 -0400

    Added some metrics directly from the DB

---
+++ lib/Metrics/SQL/GetDataMainAll.sql
@@ -0,0 +1,11 @@
+SELECT gt.fecha as date_gt,  gt.id_est as id_est, gt.val as gt, fo.val as fo
+FROM cont_otres as gt, forecast_otres as fo
+WHERE
+    date_part('year', gt.fecha)  = YEAR
+    AND date_part('year', fo.fecha)  = YEAR
+    AND fo.val = TYPEFOR
+    AND date_part('year',  fo.fecha) =  date_part('year',  gt.fecha)
+    AND date_part('month', fo.fecha) =  date_part('month', gt.fecha) 
+    AND date_part('day',   fo.fecha) =  date_part('day',   gt.fecha) 
+    AND date_part('hour',   fo.fecha) =  date_part('hour',   gt.fecha) 
+    AND gt.id_est = fo.id_est
---
+++ lib/Metrics/SQL/GetRMSEOnlyGood.sql
@@ -0,0 +1,14 @@
+SELECT count(aval) as tot, sum(sval), sum(aval)/count(aval) as mae 
+FROM 
+(	SELECT abs(gt.val - fo.val) as aval, (gt.val - fo.val)^2 as sval
+	FROM forecast_otres as fo, cont_otres as gt
+	WHERE
+	  date_part('year', fo.fecha) = 2017 AND
+	  date_part('year', gt.fecha) = 2017 AND
+	  fo.id_est = gt.id_est  AND
+	  date_part('year', fo.fecha)  = date_part('year', gt.fecha) AND
+	  date_part('month', fo.fecha)  = date_part('month', gt.fecha) AND
+	  date_part('day', fo.fecha)  = date_part('day', gt.fecha) AND
+	  date_part('hour', fo.fecha)  = date_part('hour', gt.fecha) AND
+	  fo.id_est IN ('ATI', 'BJU','CUA','LPR','MER','PED','TLA','UIZ','XAL')  
+	 ) as internal
