6d0ac46c07a12fdd673fc0a1769e8b77aa5d49e4
Pablocg0 <pablocg064@gmail.com>
2018-05-31 11:18:53 -0500

    update forecastDate and Automatic_System

--- modulos/forecast/forecast_month.sh
+++ modulos/forecast/forecast_month.sh
@@ -3,5 +3,5 @@
 source ~/anaconda3/envs/tensorflow/bin/activate tensorflow
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64/
 
-#python /ServerScript/AirQualityModel/ContaminationForecast/lib/NetCDF/makeCsv.py
+python /ServerScript/AirQualityModel/ContaminationForecast/lib/NetCDF/makeCsv.py
 python /ServerScript/AirQualityModel/ContaminationForecast/lib/forecast_month.py O3
